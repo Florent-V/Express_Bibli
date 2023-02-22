04fa9ddd299e0dd9cec0affca8134a08c3377c69
Florent-Vasseur <vasseurflorent@gmail.com>
2023-02-22 16:13:29 +0100

    nodemon & dotenv

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.SERVER_PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Something bad happened');
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
